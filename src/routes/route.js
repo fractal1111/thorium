36b0382ea1133c40f7c3467c94e2d1d8c0a6d67e
fractal1111 <parthauto4@gmail.com>
2022-02-22 18:17:38 +0530

    GET/api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,114 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+router.get('/movies',function(req,res){
+    const movie = ['Shutter Island','Titanic','Django Unchained','Revenant']
+     res.send(movie);
+
+
+
+})
+
+router.get('/movies/:index',function(req,res){
+
+
+const movie = ['Shutter Island','Titanic','Django Unchained','Revenant']
+let mn =req.params.index;
+if(mn>movie.length-1){
+    res.send('Use valid index')
+}
+else if(mn<movie.length){
+
+    let x = movie[mn]
+    res.send(x)
+}
+
+
+})
+ 
+
+router.get('/films', function(req,res){
+
+const film = [
+
+    {
+        "id":1, "name":"Shutter Island"
+    },
+   
+    {
+        "id":2, "name":"Titanic"   
+     },
+
+     {
+         "id":3, "name":"Django Unchained"
+
+
+     },
+
+     {
+        
+        "id":4 , "name":"revenant"
+
+     }
+
+
+]
+res.send(film)
+
+})
+
+
+router.get('/films/:filmsid', function(req,res){
+      
+
+    let fi=req.params.filmsid;
+    const film = [
+    
+        {
+            "id":1, "name":"Shutter Island"
+        },
+       
+        {
+            "id":2, "name":"Titanic"   
+         },
+    
+         {
+             "id":3, "name":"Django Unchained"
+    
+    
+         },
+    
+         {
+            
+            "id":4 , "name":"revenant"
+    
+         }
+    
+    
+    ]
+
+    if(fi>film.length){
+
+        res.send("invalid id")
+    }
+    else {
+
+        for(let i=0;i<film.length;i++){
+            if(film[i].id==fi){
+
+                res.send(film[i])
+            }
+
+        }
+    }
+   
+   
+   
+   
+    res.send(film)
+    
+    })
+
 
 
 
