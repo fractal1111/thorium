f3dbe843942502b236021f5f17a5cdb81a95442a
fractal1111 <parthauto4@gmail.com>
2022-03-09 00:47:10 +0530

    bettter

--- src/controllers/allControllers.js
+++ src/controllers/allControllers.js
@@ -5,7 +5,7 @@ const OrderModel = require("../models/orderModel")
 const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
-    
+
     res.send({msg: savedData})
 }
 //part 2
@@ -19,49 +19,52 @@ res.send({msg:saveData})
 
 const createOrder = async function(req,res){
     let header = req.headers.isfreeappuser
-   
+
     let uid = req.body.user
     let pid = req.body.product
     let data = req.body
-    
+
     let ui = await UserModel.find({_id:uid})
-    
+
     if(ui.length==0){res.send("Invalid User Id")}
-    
+
     let pi = await ProductModel.find({_id:pid})
     if(pi.length==0){res.send("Invalid Prosuct Id")}
 
-    let savedOrder = await OrderModel.create(data)
 
 
-   
+
+
 //if header is true
  if (header==='true'){
+    let savedOrder = await OrderModel.create(data)
     let updatedStatus = await OrderModel.updateOne({user:uid},{$set:{isFreeAppUser:true}})
     let updatedStatus1 = await UserModel.updateOne({_id:uid},{$set:{isFreeAppUser:true}})
     let finalObject1= await OrderModel.find({user:uid})
-    res.send({msg:finalObject1})
+    return res.send({msg:finalObject1})
 
 }
 //if header is false
- else {
+ if(header==='false') {
 
-    let price = await ProductModel.find({_id:pid}).select({price:1,_id:0})
-    let p = price.map(ele=>ele.price)
-    
-    let balance = await UserModel.find({_id:uid}).select({balance:1, _id:0})
-    let b = balance.map(inp=>inp.balance)
-    
+    let product = await ProductModel.findOne({_id:pid})
+    let p =product.price
+
+    let user = await UserModel.findOne({_id:uid})
+    let b = user.balance
+
     if(p[0]>b[0]){ return res.send("Insufficient Balance")}
-    
+
+    let savedOrder = await OrderModel.create(data)
+
     let newBalance = b[0]-p[0]
-    
+
     let updatedBalance = await UserModel.updateOne({_id:uid},{$set:{balance:newBalance}})
     let updatedAmount = await OrderModel.updateOne({user:uid},{$set:{amount:p[0]}})
     let finalObject= await OrderModel.find({user:uid})
-    
+
     res.send({msg:finalObject})
-    
+
     }
 }
 
